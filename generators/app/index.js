a52ae6b39dc9ee8cf29497d8a371f2557d075a0d
Timo Yan <tomyan13@gmail.com>
2019-08-13 01:27:53 +0800

    Add aspnet core files and modify script.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,6 @@
 const Generator = require('yeoman-generator');
 const mkdirp = require('mkdirp');
 const path = require('path');
-const prettier = require('prettier');
 
 module.exports = class extends Generator {
     // The name `constructor` is important here
@@ -11,8 +10,7 @@ module.exports = class extends Generator {
 
         // TODO: Will remove when promption enable
         this.answers = {
-            c_project_name: 'cs-portal',
-            c_chart_name: 'sample-chart-name',
+            c_project_name: 'sample-chart',
             c_dotnet_project_name: 'Sample.Chart.Name'
         };
     }
@@ -28,205 +26,203 @@ module.exports = class extends Generator {
     //     ]);
     // }
 
-    // generateJSON() {
-    //     const { c_project_name } = this.answers;
-    //     const c_script_name = {
-    //         install: 'install',
-    //         bootstrap: 'bootstrap',
-    //         dev: `${c_project_name}-dev`,
-    //         'build-dev': `${c_project_name}-build-dev`,
-    //         'build-preprod': `${c_project_name}-build-preprod`,
-    //         'build-prod': `${c_project_name}-build-prod`,
-    //         lint: `${c_project_name}-lint`,
-    //         'type-check': `${c_project_name}-type-check`,
-    //         test: `${c_project_name}-test`
-    //     };
-    //     const changeToProjectDir = `cd packages/${c_project_name}`;
+    generateJSON() {
+        const { c_project_name } = this.answers;
+        const c_script_name = {
+            install: 'install',
+            bootstrap: 'bootstrap',
+            dev: `${c_project_name}-dev`,
+            'build-dev': `${c_project_name}-build-dev`,
+            'build-preprod': `${c_project_name}-build-preprod`,
+            'build-prod': `${c_project_name}-build-prod`,
+            lint: `${c_project_name}-lint`,
+            'type-check': `${c_project_name}-type-check`,
+            test: `${c_project_name}-test`
+        };
+        const changeToProjectDir = `cd packages/${c_project_name}`;
 
-    //     const pkgJson = {
-    //         name: `@${c_project_name}/root`,
-    //         private: true,
-    //         version: 'independent',
-    //         description: '',
-    //         main: 'index.js',
-    //         workspaces: ['packages/*', 'packages/*/packages/*'],
-    //         scripts: {
-    //             [c_script_name.install]: 'npx lerna link',
-    //             [c_script_name.bootstrap]: 'npx lerna clean --yes && npx lerna bootstrap',
-    //             [c_script_name.dev]: `${changeToProjectDir} && yarn dev`,
-    //             [c_script_name[
-    //                 'build-preprod'
-    //             ]]: `${changeToProjectDir} && yarn build-preprod`,
-    //             [c_script_name['build-prod']]: `${changeToProjectDir} && yarn build-prod`,
-    //             [c_script_name.lint]: `${changeToProjectDir} && yarn lint`,
-    //             [c_script_name['type-check']]: `${changeToProjectDir} && yarn type-check`,
-    //             [c_script_name.test]: `${changeToProjectDir} && yarn test`
-    //         },
-    //         author: 'Timo Yan',
-    //         license: 'ISC',
-    //         devDependencies: {
-    //             lerna: '3.13.1',
-    //             'cross-env': '5.2.0',
-    //             husky: '1.3.1',
-    //             'lint-staged': '8.1.5',
-    //             prettier: '1.16.4'
-    //         },
-    //         husky: {
-    //             hooks: {
-    //                 'pre-commit': `yarn ${
-    //                     c_script_name['type-check']
-    //                 } && npx lint-staged'`,
-    //                 'pre-push': `yarn ${c_script_name.test}`
-    //             }
-    //         },
-    //         'lint-staged': {
-    //             linters: {
-    //                 '*.{ts,tsx}': ['yarn pe-lint', 'prettier --write', 'git add'],
-    //                 '*.json': ['prettier --write', 'git add']
-    //             },
-    //             ignore: ['**/dist/*.min.js', 'node_modules']
-    //         }
-    //     };
+        const pkgJson = {
+            name: `@${c_project_name}/root`,
+            private: true,
+            version: 'independent',
+            description: '',
+            main: 'index.js',
+            workspaces: ['packages/*', 'packages/*/packages/*'],
+            scripts: {
+                [c_script_name.install]: 'npx lerna link',
+                [c_script_name.bootstrap]: 'npx lerna clean --yes && npx lerna bootstrap',
+                [c_script_name.dev]: `${changeToProjectDir} && yarn dev`,
+                [c_script_name[
+                    'build-preprod'
+                ]]: `${changeToProjectDir} && yarn build-preprod`,
+                [c_script_name['build-prod']]: `${changeToProjectDir} && yarn build-prod`,
+                [c_script_name.lint]: `${changeToProjectDir} && yarn lint`,
+                [c_script_name['type-check']]: `${changeToProjectDir} && yarn type-check`,
+                [c_script_name.test]: `${changeToProjectDir} && yarn test`
+            },
+            author: 'Timo Yan',
+            license: 'ISC',
+            devDependencies: {
+                lerna: '3.13.1',
+                'cross-env': '5.2.0',
+                husky: '1.3.1',
+                'lint-staged': '8.1.5',
+                prettier: '1.16.4'
+            },
+            husky: {
+                hooks: {
+                    'pre-commit': `yarn ${
+                        c_script_name['type-check']
+                    } && npx lint-staged'`,
+                    'pre-push': `yarn ${c_script_name.test}`
+                }
+            },
+            'lint-staged': {
+                linters: {
+                    '*.{ts,tsx}': ['yarn pe-lint', 'prettier --write', 'git add'],
+                    '*.json': ['prettier --write', 'git add']
+                },
+                ignore: ['**/dist/*.min.js', 'node_modules']
+            }
+        };
 
-    //     // Extend or create package.json file in destination path
-    //     this.fs.writeJSON(this.destinationPath('package.json'), pkgJson);
-    // }
+        // Extend or create package.json file in destination path
+        this.fs.writeJSON(this.destinationPath('package.json'), pkgJson);
+    }
 
-    // copyRootFiles() {
-    //     this.fs.copy(this.templatePath('root/**'), this.destinationRoot(), {
-    //         globOptions: { dot: true }
-    //     });
-    // }
+    copyRootFiles() {
+        this.fs.copy(this.templatePath('root/**'), this.destinationRoot(), {
+            globOptions: { dot: true }
+        });
+    }
 
-    // install() {
-    //     this.yarnInstall(null, { registry: 'https://registry.npm.taobao.org/' }, null);
-    // }
+    install() {
+        this.yarnInstall(null, { registry: 'https://registry.npm.taobao.org/' }, null);
+    }
 
-    // createPackages() {
-    //     mkdirp.sync(this.destinationPath('packages'));
-    // }
+    createPackages() {
+        mkdirp.sync(this.destinationPath('packages'));
+    }
 
-    // createProjectPackage() {
-    //     const { c_project_name } = this.answers;
-    //     const packagesPath = this.destinationPath('packages');
-    //     mkdirp.sync(path.join(packagesPath, c_project_name));
-    // }
+    createProjectPackage() {
+        const { c_project_name } = this.answers;
+        const packagesPath = this.destinationPath('packages');
+        mkdirp.sync(path.join(packagesPath, c_project_name));
+    }
 
-    // createFilesAndFoldersOfProject() {
-    //     const { c_project_name } = this.answers;
-    //     const packagesPath = this.destinationPath('packages');
-    //     const projectPackagePath = path.join(packagesPath, c_project_name);
-    //     const projectProjectPackagePath = path.join(projectPackagePath, 'packages');
+    createFilesAndFoldersOfProject() {
+        const { c_project_name } = this.answers;
+        const packagesPath = this.destinationPath('packages');
+        const projectPackagePath = path.join(packagesPath, c_project_name);
+        const projectProjectPackagePath = path.join(projectPackagePath, 'packages');
 
-    //     mkdirp.sync(projectProjectPackagePath);
+        mkdirp.sync(projectProjectPackagePath);
 
-    //     const subPackagesDirName = [
-    //         'apis',
-    //         'client',
-    //         'components',
-    //         'config',
-    //         'constants',
-    //         'containers',
-    //         'epics',
-    //         'reducers',
-    //         'tests',
-    //         'types',
-    //         'utilities',
-    //         'validations'
-    //     ];
+        const subPackagesDirName = [
+            'apis',
+            'client',
+            'components',
+            'config',
+            'constants',
+            'containers',
+            'epics',
+            'reducers',
+            'tests',
+            'types',
+            'utilities',
+            'validations'
+        ];
 
-    //     subPackagesDirName.forEach(dirName => {
-    //         this._private_createSubPackageDirAndFiles(dirName, projectProjectPackagePath);
-    //     });
-
-    //     this.fs.copyTpl(
-    //         this.templatePath('project/**'),
-    //         projectPackagePath,
-    //         { project_name: c_project_name },
-    //         null,
-    //         {
-    //             globOptions: { dot: true }
-    //         }
-    //     );
-    //     this.fs.copyTpl(
-    //         this.templatePath('public/index.html'),
-    //         path.join(projectPackagePath, 'public/index.html'),
-    //         {
-    //             project_name: c_project_name,
-    //             webpack_settings: `
-    //     <% _.forEach(htmlWebpackPlugin.files.js, function(js) { %>
-    //     <script type="text/javascript" src="<%-js%>"></script>
-    //     <% }); _.forEach(htmlWebpackPlugin.files.css, function(css) { %>
-    //     <link rel="stylesheet" type="text/css" href="<%-css%>" /> <% }); %>
-    //             `
-    //         }
-    //     );
-    // }
-
-    // _private_createSubPackageDirAndFiles(dirName, parentDirPath) {
-    //     const { c_project_name } = this.answers;
-    //     const dirPath = path.join(parentDirPath, dirName);
-    //     mkdirp.sync(dirPath);
-
-    //     const pkgJSON = {
-    //         name: `@${c_project_name}/${dirName}`,
-    //         version: '0.1.0',
-    //         description: '',
-    //         author: 'Timo Yan',
-    //         license: 'ISC'
-    //     };
-
-    //     this.fs.writeJSON(path.join(dirPath, 'package.json'), pkgJSON);
-    //     this.fs.copy(
-    //         this.templatePath('sub-package/index.ts'),
-    //         path.join(dirPath, 'index.ts')
-    //     );
-    // }
-
-    // createVSCodeConfigurations() {
-    //     const { c_project_name } = this.answers;
-
-    //     this.fs.copyTpl(
-    //         this.templatePath('vscode/**'),
-    //         this.destinationPath('./.vscode'),
-    //         { project_name: c_project_name },
-    //         null,
-    //         {
-    //             globOptions: {
-    //                 dot: true
-    //             }
-    //         }
-    //     );
-    // }
-    createDotNetCoreProject() {
-        const { c_dotnet_project_name } = this.answers;
-        const aspnetTemplatePath = this.templatePath('aspnet');
-        const aspnetFolderPath = this.destinationPath(c_dotnet_project_name);
-        // this.spawnCommandSync(
-        //     `dotnet new web -n ${c_dotnet_project_name} --no-restore`,
-        //     [],
-        //     {
-        //         stdio: 'inherit',
-        //         cwd: this.destinationPath()
-        //     }
-        // );
-        mkdirp.sync(aspnetFolderPath);
-        this.fs.copy(
-            path.join(aspnetTemplatePath, 'template.csproj'),
-            path.join(aspnetFolderPath, `${c_dotnet_project_name}.csproj`)
-        );
+        subPackagesDirName.forEach(dirName => {
+            this._private_createSubPackageDirAndFiles(dirName, projectProjectPackagePath);
+        });
 
         this.fs.copyTpl(
-            path.join(aspnetTemplatePath, '**'),
+            this.templatePath('project/**'),
+            projectPackagePath,
+            { project_name: c_project_name },
+            null,
+            {
+                globOptions: { dot: true }
+            }
+        );
+        this.fs.copyTpl(
+            this.templatePath('public/index.html'),
+            path.join(projectPackagePath, 'public/index.html'),
+            {
+                project_name: c_project_name,
+                webpack_settings: `
+        <% _.forEach(htmlWebpackPlugin.files.js, function(js) { %>
+        <script type="text/javascript" src="<%-js%>"></script>
+        <% }); _.forEach(htmlWebpackPlugin.files.css, function(css) { %>
+        <link rel="stylesheet" type="text/css" href="<%-css%>" /> <% }); %>
+                `
+            }
+        );
+    }
+
+    _private_createSubPackageDirAndFiles(dirName, parentDirPath) {
+        const { c_project_name } = this.answers;
+        const dirPath = path.join(parentDirPath, dirName);
+        mkdirp.sync(dirPath);
+
+        const pkgJSON = {
+            name: `@${c_project_name}/${dirName}`,
+            version: '0.1.0',
+            description: '',
+            author: 'Timo Yan',
+            license: 'ISC'
+        };
+
+        this.fs.writeJSON(path.join(dirPath, 'package.json'), pkgJSON);
+        this.fs.copy(
+            this.templatePath('sub-package/index.ts'),
+            path.join(dirPath, 'index.ts')
+        );
+    }
+
+    createVSCodeConfigurations() {
+        const { c_project_name } = this.answers;
+
+        this.fs.copyTpl(
+            this.templatePath('vscode/**'),
+            this.destinationPath('./.vscode'),
+            { project_name: c_project_name },
+            null,
+            {
+                globOptions: {
+                    dot: true
+                }
+            }
+        );
+    }
+
+    createDotNetCoreProject() {
+        const { c_dotnet_project_name, c_project_name } = this.answers;
+        const aspnetTemplatePath = this.templatePath('aspnet');
+        const aspnetFolderPath = this.destinationPath(c_dotnet_project_name);
+
+        mkdirp.sync(aspnetFolderPath);
+
+        this.fs.copyTpl(
+            aspnetTemplatePath,
             aspnetFolderPath,
-            { c_aspnet_project_name: c_dotnet_project_name },
+            {
+                c_aspnet_project_name: c_dotnet_project_name,
+                c_project_name: c_project_name
+            },
             null,
             {
                 globOptions: {
                     dot: true,
-                    ignore: [path.join(aspnetTemplatePath, 'template.csproj')]
+                    ignore: ['**/*.csproj']
                 }
             }
+        );
+
+        this.fs.copy(
+            path.join(aspnetTemplatePath, 'template.csproj'),
+            path.join(aspnetFolderPath, `${c_dotnet_project_name}.csproj`)
         );
     }
 };
---
+++ generators/app/templates/aspnet/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+/* fixformat ignore:start */
+namespace <%= c_aspnet_project_name %>.Controllers 
+/* fixformat ignore:end */
+{
+    public class HomeController : Controller {
+        public HomeController (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public async Task<ActionResult> Index () {
+            return View ();
+        }
+
+        /// <summary>
+        /// This is for health checking used by DevOps
+        /// </summary>
+        /// <returns></returns>
+        [Route ("APIHealthCheck")]
+        public IActionResult HealthCheck () {
+            return Ok ();
+        }
+    }
+}
